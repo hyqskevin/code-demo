ea1437d32178d04b53f31ad25eac2ef679dc29d6
hyqskevin <hyqskevin@gmail.com>
2021-08-05 15:32:06 +0800

    update visualization demo
    数据可视化demo

---
+++ 数据可视化demo/readme.md
@@ -0,0 +1,25 @@
+## 数据可视化
+
+常用 matplotlib 和 seaborn 可视化图表
+
+- 散点图/折线图/条形图/箱线图/饼图/热力图/蜘蛛图
+- 二元变量分布图/成对关系图
+
+### Titanic 数据可视化实例
+
+实现操作:
+
+- 数据清洗
+- pandas.Series.str.get_dummies() 进行 one-hot 编码
+- seaborn heatmap 展示皮尔森相关系数
+- 特征重要性展示
+- 决策树可视化展示
+
+### 词云可视化
+
+- nltk wordtokenizer 实现分词
+- word cloud 生成词云
+
+### pyEcharts 可视化
+
+- 使用 echarts 格式进行数据可视化操作
---
+++ 数据可视化demo/titanic_demo.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn import metrics
+
+# 决策树可视化
+import pydotplus
+from six import StringIO
+from sklearn.tree import export_graphviz
+
+# 数据加载
+train_data = pd.read_csv('./train.csv')
+test_data = pd.read_csv('./test.csv')
+
+# 查看空值
+# print(train_data.isnull().any())
+# print(train_data['Embarked'].value_counts())
+
+# 使用平均年龄来填充年龄中的nan值
+train_data['Age'].fillna(train_data['Age'].mean(), inplace=True)
+test_data['Age'].fillna(test_data['Age'].mean(), inplace=True)
+# 使用票价的均值填充票价中的nan值
+train_data['Fare'].fillna(train_data['Fare'].mean(), inplace=True)
+test_data['Fare'].fillna(test_data['Fare'].mean(), inplace=True)
+# 使用登录最多的港口来填充登录港口的nan值
+train_data['Embarked'].fillna('S', inplace=True)
+test_data['Embarked'].fillna('S', inplace=True)
+
+# 特征选择
+features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
+train_features = train_data[features]
+
+# 显示特征之间的相关系数
+plt.figure('Fig1', figsize=(12, 12))
+plt.title('Pearson Correlation between Features', y=1.05, size=15)
+# Embarked 和 Sex 使用 pandas.Series.str.get_dummies() 进行one-hot编码
+train_data_hot_encoded = train_features.drop('Embarked', 1) \
+    .join(train_features.Embarked.str.get_dummies())
+train_data_hot_encoded = train_data_hot_encoded.drop('Sex', 1) \
+    .join(train_data_hot_encoded.Sex.str.get_dummies())
+# 计算特征之间的Pearson系数，即相似度
+sns.heatmap(train_data_hot_encoded.corr(), linewidths=0.1, vmax=1.0,
+            fmt='.2f', square=True, linecolor='white', annot=True)
+plt.show()
+
+
+#
+# # 使用饼图来进行Survived取值的可视化
+# train_data["Survived"].value_counts().plot(kind="pie", label='Survived')
+# plt.show()
+#
+# # 不同的Pclass,幸存人数(条形图)
+# sns.barplot(x='Pclass', y="Survived", data=train_data)
+# plt.show()
+#
+# # 不同的Embarked,幸存人数(条形图)
+# sns.barplot(x='Embarked', y="Survived", data=train_data)
+# plt.show()
+
+
+# 训练并显示特征向量的重要程度
+def train(train_features, train_labels):
+    # 构造CART决策树
+    clf = DecisionTreeClassifier()
+    clf.fit(train_features, train_labels)
+
+    # 显示特征向量的重要程度
+    coeffs = clf.feature_importances_
+    df_co = pd.DataFrame(coeffs, columns=["importance"])
+    # 下标设置为Feature Name
+    df_co.index = train_features.columns
+    # print(df_co)
+
+    df_co.sort_values("importance", ascending=True, inplace=True)
+    df_co.plot(kind="barh")
+    plt.title("Feature Importance")
+    plt.legend(loc='lower right')
+    plt.show()
+
+    return clf
+
+
+clf = train(train_data_hot_encoded, train_data["Survived"])
+
+
+def show_tree(clf):
+    dot_data = StringIO()
+    export_graphviz(clf, out_file=dot_data)
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    # graph.write_pdf("titanic_tree.pdf")
+
+
+show_tree(clf)
